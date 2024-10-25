44d7b1ca3bf9e43a82de8e76a64bdeaaad011d55
rahulsinghpython <rahulsinghpython@gmail.com>
2024-10-25 17:18:16 +0800

    Carousell not loading assets in server, testing if dirname is the issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths({ root: __dirname }), react()],
   build: {
     rollupOptions: {
       output: {
